bdc627432ff849916ab326b93adca8ace4d66d37
Fadli Saad <fadlisaad@gmail.com>
2023-06-19 14:12:09 +0800

    Update payment processing

--- php/payment.php
+++ php/payment.php
@@ -40,10 +40,11 @@ class Payment
             );
 
             $transaction_extra = array(
-                'nama' => $data['CUSTOMER_NAME'],
-                'id' => $data['CUSTOMER_ID'],
-                'telefon' => $data['CUSTOMER_MOBILE'],
-                'email' => $data['CUSTOMER_EMAIL']
+                'nama_pelanggan' => $data['CUSTOMER_NAME'],
+                'id_pelanggan' => $data['CUSTOMER_ID'],
+                'telefon_pelanggan' => $data['CUSTOMER_MOBILE'],
+                'email_pelanggan' => $data['CUSTOMER_EMAIL'],
+                'keterangan_transaksi' => $data['TXN_DESC']
             );
 
             $encrypt = new StringerController();
@@ -61,9 +62,9 @@ class Payment
             $fpx_data = array(
                 'TRANS_ID' => $transaction_id,
                 'AMOUNT' => $transaction_data['amount'],
-                'PAYEE_NAME' => $transaction_extra['nama'],
-                'PAYEE_EMAIL' => $transaction_extra['email'],
-                'EMAIL' => $transaction_extra['email'],
+                'PAYEE_NAME' => $transaction_extra['nama_pelanggan'],
+                'PAYEE_EMAIL' => $transaction_extra['email_pelanggan'],
+                'EMAIL' => $transaction_extra['email_pelanggan'],
                 'PAYMENT_MODE' => $transaction_data['payment_mode'],
                 'BANK_CODE' => $data['BANK_CODE'],
                 'BE_MESSAGE' => $data['BE_MESSAGE'],
@@ -94,23 +95,59 @@ class Payment
     {
         $input = $_POST;
 
-        $fpx_data = [
-            'status' => isset($_POST['STATUS']) ? $_POST['STATUS'] : NULL,
-            'status_code' => isset($_POST['STATUS_CODE']) ? $_POST['STATUS_CODE'] : NULL,
-            'status_message' => isset($_POST['STATUS_MESSAGE']) ? $_POST['STATUS_MESSAGE'] : NULL,
-            'payment_datetime' => $_POST['PAYMENT_DATETIME'],
-            'payment_mode' => $_POST['PAYMENT_MODE'],
-            'amount' => $_POST['AMOUNT'],
-            'payment_transaction_id' => $_POST['PAYMENT_TRANS_ID'],
-            'buyer_bank' => $_POST['BUYER_BANK'],
-            'merchant_order_no' => $_POST['MERCHANT_ORDER_NO'],
-            'payment_transaction_id' => $_POST['APPROVAL_CODE'],
-            'trans_id' => $_POST['TRANS_ID'],
-            'approval_code' => $_POST['APPROVAL_CODE'],
-            'buyer_bank' => $_POST['BUYER_BANK'],
-            'buyer_name' => $_POST['BUYER_NAME']
-        ];
+        if($_POST['STATUS'] == '1'){
+            // payment is success
 
-        return json_encode($fpx_data);
+            $fpx_data['payment_details'] = [
+                'status' => isset($_POST['STATUS']) ? $_POST['STATUS'] : NULL,
+                'status_code' => isset($_POST['STATUS_CODE']) ? $_POST['STATUS_CODE'] : NULL,
+                'status_message' => isset($_POST['STATUS_MESSAGE']) ? $_POST['STATUS_MESSAGE'] : NULL,
+                'payment_datetime' => $_POST['PAYMENT_DATETIME'],
+                'payment_mode' => $_POST['PAYMENT_MODE'],
+                'amount' => $_POST['AMOUNT'],
+                'payment_transaction_id' => $_POST['PAYMENT_TRANS_ID'],
+                'buyer_bank' => $_POST['BUYER_BANK'],
+                'merchant_order_no' => $_POST['MERCHANT_ORDER_NO'],
+                'payment_transaction_id' => $_POST['APPROVAL_CODE'],
+                'trans_id' => $_POST['TRANS_ID'],
+                'approval_code' => $_POST['APPROVAL_CODE'],
+                'buyer_bank' => $_POST['BUYER_BANK'],
+                'buyer_name' => $_POST['BUYER_NAME']
+            ];
+
+            $fpx_data['customer_details'] = array(
+                'CUSTOMER_NAME' => $_POST['nama_pelanggan'],
+                'CUSTOMER_ID' => $_POST['id_pelanggan'],
+                'CUSTOMER_MOBILE' => $_POST['telefon_pelanggan'],
+                'CUSTOMER_EMAIL' => $_POST['email_pelanggan'],
+                'TXN_DESC' => $_POST['keterangan_transaksi']
+            );
+
+            $data = json_encode($fpx_data);
+
+            // redirect to receipt page
+            echo "<form id=\"receipt\" action=\"success.php\" method=\"post\">";
+            foreach ($_POST as $a => $b) {
+                echo '<input type="hidden" name="'.htmlentities($a).'" value="'.filter_var($b, FILTER_SANITIZE_STRING).'">';
+            }
+            echo '<input type="hidden" name="payload" value="'.base64_encode('eb4yAr').'">';
+            echo "</form>";
+            echo "<script type=\"text/javascript\">
+                document.getElementById('receipt').submit();
+            </script>";
+
+        } else {
+
+            // payment is failed, redirect to error page
+            echo "<form id=\"receipt\" action=\"failed.php\" method=\"post\">";
+            foreach ($input as $a => $b) {
+                echo '<input type="hidden" name="'.htmlentities($a).'" value="'.filter_var($b, FILTER_SANITIZE_STRING).'">';
+            }
+            echo '<input type="hidden" name="payload" value="'.base64_encode('eb4yAr').'">';
+            echo "</form>";
+            echo "<script type=\"text/javascript\">
+                document.getElementById('receipt').submit();
+            </script>";
+        }
     }
 }
